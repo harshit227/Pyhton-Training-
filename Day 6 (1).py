a94ee8d3f3890ab89d34e8e8174e7ee041ce9997
harshit227 <51996431+harshit227@users.noreply.github.com>
2019-06-22 11:08:30 +0530

    Add files via upload

---
+++ Day 6 (1).py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+x=10
+if(x<15):
+    print('Number is less than 15')
+
+
+# In[26]:
+
+
+x=10
+if(x>15):
+    print('hello there')
+else:
+    print('bye')
+
+
+# In[35]:
+
+
+x=10
+y=222
+z=100
+if(x>y and x>z):
+    print('x is greater')
+if(y>x and y>z):
+    print('y is greater')
+else:
+    print('z is greater')
+
+
+# In[37]:
+
+
+x=20
+y=20
+if(x==y):
+    print(x*x)
+else:
+    print(x*y)
+
+
+# In[42]:
+
+
+x=-10
+if(x==0):
+    print('zero')
+elif(x>0):
+    print('postive')
+else:
+    print('negative')
+
+
+# In[44]:
+
+
+n=1
+while(n<=10):
+    print(n)
+    n=n+1
+
+
+# In[46]:
+
+
+n=10
+while(n>=1):
+    print(n)
+    n=n-1
+
+
+# In[47]:
+
+
+n=0
+while(n<=10):
+    print(n)
+    n=n+2
+
+
+# In[48]:
+
+
+n=-22
+while(n>=-45):
+    print(n)
+    n=n-1
+
+
+# In[53]:
+
+
+n=0
+sum=0
+while(n<=100):
+    sum=sum+n
+    n=n+2
+    print(sum)
+
+
+# In[ ]:
+
+
+
+
